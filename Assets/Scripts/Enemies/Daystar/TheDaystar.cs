32a4cca64bd933fcb87c77e8c8b3d764365720da
Khiemnd <khiemnd777@gmail.com>
2019-09-19 07:57:36 +0700

    init probuilder

---
+++ Assets/Scripts/Enemies/Daystar/TheDaystar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheDaystar : MonoBehaviour
+{
+    HealthPoint _hp;
+
+    void Awake ()
+    {
+        _hp = GetComponent<HealthPoint> ();
+        _hp.OnDeath += OnDeath;
+    }
+
+    void OnDeath ()
+    {
+        Debug.Log (name + " is dead.");
+    }
+}
---
+++ Assets/Scripts/Utilities/HealthPoint.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPoint : MonoBehaviour
+{
+    [SerializeField]
+    float _hp;
+    [SerializeField]
+    float _maxHp;
+    public event System.Action OnDeath;
+
+    public float hp
+    {
+        get
+        {
+            return _hp;
+        }
+    }
+
+    public float maxHp
+    {
+        get
+        {
+            return _maxHp;
+        }
+    }
+
+    public void TakeDamage (float damage)
+    {
+        _hp -= damage;
+        if (_hp > 0) return;
+        if (OnDeath != null)
+        {
+            OnDeath ();
+            return;
+        }
+    }
+
+    public void Recovery (float amount)
+    {
+        _hp += amount;
+        if (_hp < _maxHp) return;
+        _hp = _maxHp;
+    }
+
+    public void SetMaxHp (float amount)
+    {
+        _maxHp = amount;
+    }
+}
