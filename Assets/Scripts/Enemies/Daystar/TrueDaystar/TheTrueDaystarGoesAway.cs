bc8b10bcd525b909357c8752224cdd72a9c0ee88
Khiemnd <khiemnd777@gmail.com>
2019-10-03 22:48:04 +0700

    the true daystar goes away

---
+++ Assets/Scripts/Enemies/Daystar/TrueDaystar/TheTrueDaystarGoesAway.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheTrueDaystarGoesAway : MonoBehaviour
+{
+    [SerializeField]
+    float _deltaForce;
+    Gyroscope _gyro;
+    Rigidbody _rigid;
+
+    void Awake ()
+    {
+        _rigid = GetComponent<Rigidbody> ();
+    }
+
+    void Start ()
+    {
+        _gyro = Input.gyro;
+        _gyro.enabled = true;
+    }
+
+    void Update ()
+    {
+        var tilt = Input.acceleration;
+        if (tilt.y > 0)
+        {
+            _rigid.AddForce (tilt * _deltaForce);
+        }
+        else
+        {
+            _rigid.velocity = Vector3.zero;
+        }
+    }
+}
